7d063dac059b083163120bffbb8bdc4aef6f96c6
Felix Duvallet <felixd@gmail.com>
2017-12-18 09:07:30 -0500

    part 2

--- day5.py
+++ day5.py
@@ -15,18 +15,24 @@ def convert(string):
     return s
 
 
-def execute(string):
+def execute(string, simple=True):
     index = 0
     count = 0
 
     numbers = convert(string)
 
     while 0 <= index < len(numbers):
-        if count > 100 and count% 1000 == 0:
+        if count < 100 or count % 1000 == 0:
             print('[{}, {}] - {}'.format(index, count, numbers))
 
         delta = numbers[index]
-        numbers[index] += 1
+
+        offset = 1
+
+        if not simple and delta >= 3:
+            offset = -1
+
+        numbers[index] += offset
         index += delta
 
         count += 1
@@ -43,9 +49,16 @@ class Day5(unittest.TestCase):
     def test_execute_data(self):
         self.assertEqual(373160, execute(day5_data))
 
+    def test_execute_complex(self):
+        self.assertEqual(10, execute(example_input, False))
+
+    def ztest_execute_data_complex(self):
+        self.assertEqual(373160, execute(day5_data, False))
+
 
 def run():
-    ret = execute(day5_data)
+    #ret = execute(day5_data, False)
+    ret = execute(example_input, False)
     print(ret)
 
 if __name__ == '__main__':
